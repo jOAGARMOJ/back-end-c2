0a9439879b773ebf45c803af2df2b907318bcfee
jOAGARMOJ <ja.garcia19@ciaf.edu.co>
2025-10-18 11:50:25 -0500

    se ingresa codigo basico para confirmar que el codigo funciona correctamente

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
+
+
