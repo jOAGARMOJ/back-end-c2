1a4a02856a488d3826ad1176a8748162e1acf198
jOAGARMOJ <ja.garcia19@ciaf.edu.co>
2025-10-18 12:09:13 -0500

    se crea ruta raiz en el index

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
 });
 
+// Ruta raíz que responde con un mensaje JSON
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'API de usuarios funcionando correctamente ',
+        timestamp: new Date().toISOString(),
+        status: 'Success'
+    });
+});
 
