71e1538694b4fb5d1922ffba1f3e2d535733ab0c
jOAGARMOJ <ja.garcia19@ciaf.edu.co>
2025-10-18 11:57:31 -0500

    se ingresa la informacion de los usser en el index

---
+++ data/Users.js
@@ -0,0 +1,85 @@
+const users = [
+    {
+        "id": "01",
+        "first_name": "Imelda",
+        "phone": "848-271-1854",
+        "email": "iwillmer0@arstechnica.com",
+        "address": "17th Floor",
+        "age": 1,
+        "photoUrl": "http://chron.com/eget/eleifend/luctus/ultricies/eu/nibh/quisque.html?in=lorem&faucibus=quisque&orci=ut&luctus=erat&et=curabitur&ultrices=gravida&posuere=nisi&cubilia=at&curae=nibh&duis=in&faucibus=hac&accumsan=habitasse&odio=platea&curabitur=dictumst&convallis=aliquam&duis=augue&consequat=quam&dui=sollicitudin&nec=vitae&nisi=consectetuer&volutpat=eget&eleifend=rutrum&donec=at&ut=lorem&dolor=integer&morbi=tincidunt&vel=ante&lectus=vel&in=ipsum&quam=praesent&fringilla=blandit&rhoncus=lacinia&mauris=erat&enim=vestibulum&leo=sed&rhoncus=magna&sed=at&vestibulum=nunc&sit=commodo&amet=placerat&cursus=praesent&id=blandit&turpis=nam&integer=nulla&aliquet=integer&massa=pede&id=justo&lobortis=lacinia&convallis=eget&tortor=tincidunt&risus=eget&dapibus=tempus&augue=vel&vel=pede&accumsan=morbi&tellus=porttitor&nisi=lorem&eu=id&orci=ligula&mauris=suspendisse&lacinia=ornare&sapien=consequat&quis=lectus&libero=in&nullam=est&sit=risus&amet=auctor&turpis=sed&elementum=tristique&ligula=in&vehicula=tempus&consequat=sit&morbi=amet&a=sem&ipsum=fusce&integer=consequat&a=nulla&nibh=nisl&in=nunc&quis=nisl&justo=duis&maecenas=bibendum&rhoncus=felis&aliquam=sed&lacus=interdum&morbi=venenatis"
+    }, {
+        "id": "02",
+        "first_name": "Gloriana",
+        "phone": "959-501-9535",
+        "email": "gwhyard1@skype.com",
+        "address": "Suite 37",
+        "age": 1,
+        "photoUrl": "http://prnewswire.com/erat/tortor/sollicitudin/mi/sit/amet/lobortis.jsp?pede=at&posuere=nibh&nonummy=in&integer=hac&non=habitasse&velit=platea&donec=dictumst&diam=aliquam&neque=augue"
+    }, {
+        "id": "03",
+        "first_name": "Tori",
+        "phone": "650-967-1347",
+        "email": "tcorcoran2@zdnet.com",
+        "address": "PO Box 12985",
+        "age": 7,
+        "photoUrl": "http://bing.com/blandit/non/interdum/in/ante/vestibulum.json?erat=tortor&fermentum=id&justo=nulla&nec=ultrices&condimentum=aliquet&neque=maecenas&sapien=leo&placerat=odio&ante=condimentum&nulla=id&justo=luctus&aliquam=nec&quis=molestie&turpis=sed&eget=justo&elit=pellentesque&sodales=viverra&scelerisque=pede&mauris=ac&sit=diam&amet=cras&eros=pellentesque&suspendisse=volutpat&accumsan=dui&tortor=maecenas&quis=tristique&turpis=est&sed=et&ante=tempus&vivamus=semper&tortor=est&duis=quam&mattis=pharetra&egestas=magna&metus=ac&aenean=consequat&fermentum=metus&donec=sapien&ut=ut&mauris=nunc&eget=vestibulum&massa=ante&tempor=ipsum&convallis=primis&nulla=in&neque=faucibus&libero=orci&convallis=luctus&eget=et&eleifend=ultrices&luctus=posuere&ultricies=cubilia&eu=curae&nibh=mauris&quisque=viverra&id=diam&justo=vitae&sit=quam&amet=suspendisse&sapien=potenti&dignissim=nullam&vestibulum=porttitor&vestibulum=lacus&ante=at&ipsum=turpis&primis=donec&in=posuere&faucibus=metus&orci=vitae&luctus=ipsum&et=aliquam&ultrices=non&posuere=mauris&cubilia=morbi&curae=non&nulla=lectus&dapibus=aliquam&dolor=sit&vel=amet&est=diam&donec=in&odio=magna&justo=bibendum&sollicitudin=imperdiet&ut=nullam&suscipit=orci&a=pede&feugiat=venenatis&et=non&eros=sodales&vestibulum=sed&ac=tincidunt&est=eu&lacinia=felis&nisi=fusce&venenatis=posuere&tristique=felis&fusce=sed&congue=lacus&diam=morbi"
+    }, {
+        "id": "04",
+        "first_name": "Ronny",
+        "phone": "830-716-6484",
+        "email": "rrihosek3@paginegialle.it",
+        "address": "Apt 1357",
+        "age": 8,
+        "photoUrl": "http://economist.com/nec/dui/luctus/rutrum/nulla/tellus.jpg?ut=ligula&nunc=vehicula&vestibulum=consequat&ante=morbi&ipsum=a&primis=ipsum&in=integer&faucibus=a&orci=nibh&luctus=in&et=quis&ultrices=justo&posuere=maecenas&cubilia=rhoncus&curae=aliquam&mauris=lacus&viverra=morbi&diam=quis&vitae=tortor&quam=id&suspendisse=nulla&potenti=ultrices&nullam=aliquet&porttitor=maecenas&lacus=leo&at=odio&turpis=condimentum&donec=id&posuere=luctus&metus=nec&vitae=molestie&ipsum=sed&aliquam=justo&non=pellentesque&mauris=viverra&morbi=pede&non=ac&lectus=diam&aliquam=cras&sit=pellentesque&amet=volutpat&diam=dui&in=maecenas&magna=tristique&bibendum=est&imperdiet=et&nullam=tempus&orci=semper&pede=est&venenatis=quam&non=pharetra&sodales=magna&sed=ac&tincidunt=consequat&eu=metus&felis=sapien&fusce=ut&posuere=nunc&felis=vestibulum"
+    }, {
+        "id": "05",
+        "first_name": "Alain",
+        "phone": "846-190-1002",
+        "email": "agibbeson4@livejournal.com",
+        "address": "Apt 1346",
+        "age": 9,
+        "photoUrl": "https://ning.com/orci/nullam.png?pellentesque=risus&viverra=dapibus&pede=augue&ac=vel&diam=accumsan&cras=tellus&pellentesque=nisi&volutpat=eu&dui=orci&maecenas=mauris&tristique=lacinia&est=sapien&et=quis&tempus=libero&semper=nullam&est=sit&quam=amet&pharetra=turpis&magna=elementum&ac=ligula&consequat=vehicula&metus=consequat&sapien=morbi&ut=a&nunc=ipsum&vestibulum=integer&ante=a&ipsum=nibh&primis=in&in=quis&faucibus=justo&orci=maecenas&luctus=rhoncus&et=aliquam&ultrices=lacus&posuere=morbi&cubilia=quis&curae=tortor&mauris=id&viverra=nulla&diam=ultrices&vitae=aliquet&quam=maecenas&suspendisse=leo&potenti=odio&nullam=condimentum&porttitor=id&lacus=luctus&at=nec&turpis=molestie&donec=sed&posuere=justo&metus=pellentesque&vitae=viverra&ipsum=pede&aliquam=ac&non=diam&mauris=cras&morbi=pellentesque&non=volutpat&lectus=dui&aliquam=maecenas&sit=tristique&amet=est&diam=et&in=tempus&magna=semper&bibendum=est&imperdiet=quam"
+    }, {
+        "id": "06",
+        "first_name": "Trescha",
+        "phone": "906-938-7121",
+        "email": "tjanecki5@linkedin.com",
+        "address": "Suite 78",
+        "age": 5,
+        "photoUrl": "https://phpbb.com/adipiscing/molestie/hendrerit/at/vulputate/vitae/nisl.jpg?convallis=sit&nunc=amet&proin=eros&at=suspendisse&turpis=accumsan&a=tortor&pede=quis&posuere=turpis&nonummy=sed&integer=ante&non=vivamus&velit=tortor&donec=duis&diam=mattis&neque=egestas&vestibulum=metus&eget=aenean&vulputate=fermentum&ut=donec&ultrices=ut&vel=mauris&augue=eget&vestibulum=massa&ante=tempor&ipsum=convallis&primis=nulla&in=neque&faucibus=libero&orci=convallis&luctus=eget&et=eleifend&ultrices=luctus&posuere=ultricies&cubilia=eu&curae=nibh&donec=quisque&pharetra=id&magna=justo&vestibulum=sit&aliquet=amet&ultrices=sapien&erat=dignissim&tortor=vestibulum&sollicitudin=vestibulum&mi=ante&sit=ipsum&amet=primis&lobortis=in&sapien=faucibus&sapien=orci&non=luctus&mi=et&integer=ultrices&ac=posuere&neque=cubilia&duis=curae&bibendum=nulla&morbi=dapibus&non=dolor&quam=vel&nec=est&dui=donec&luctus=odio&rutrum=justo&nulla=sollicitudin&tellus=ut&in=suscipit&sagittis=a&dui=feugiat&vel=et&nisl=eros&duis=vestibulum&ac=ac&nibh=est&fusce=lacinia&lacus=nisi&purus=venenatis&aliquet=tristique&at=fusce&feugiat=congue&non=diam&pretium=id&quis=ornare&lectus=imperdiet&suspendisse=sapien"
+    }, {
+        "id": "07",
+        "first_name": "Bill",
+        "phone": "556-740-3788",
+        "email": "bdrinkeld6@go.com",
+        "address": "Room 1763",
+        "age": 2,
+        "photoUrl": "http://dropbox.com/eleifend/pede/libero.js?dapibus=eu&dolor=interdum&vel=eu&est=tincidunt&donec=in&odio=leo&justo=maecenas&sollicitudin=pulvinar&ut=lobortis&suscipit=est&a=phasellus&feugiat=sit&et=amet&eros=erat&vestibulum=nulla&ac=tempus&est=vivamus&lacinia=in&nisi=felis&venenatis=eu&tristique=sapien&fusce=cursus&congue=vestibulum&diam=proin&id=eu&ornare=mi&imperdiet=nulla&sapien=ac&urna=enim&pretium=in&nisl=tempor&ut=turpis&volutpat=nec&sapien=euismod&arcu=scelerisque&sed=quam&augue=turpis&aliquam=adipiscing&erat=lorem&volutpat=vitae&in=mattis&congue=nibh&etiam=ligula&justo=nec&etiam=sem&pretium=duis&iaculis=aliquam&justo=convallis"
+    }, {
+        "id": "08",
+        "first_name": "Hayyim",
+        "phone": "494-311-2190",
+        "email": "hbelchem7@netscape.com",
+        "address": "PO Box 21776",
+        "age": 2,
+        "photoUrl": "http://themeforest.net/mi/integer/ac/neque/duis/bibendum/morbi.js?malesuada=congue&in=etiam&imperdiet=justo&et=etiam&commodo=pretium&vulputate=iaculis&justo=justo&in=in&blandit=hac&ultrices=habitasse&enim=platea&lorem=dictumst&ipsum=etiam&dolor=faucibus&sit=cursus&amet=urna&consectetuer=ut&adipiscing=tellus&elit=nulla&proin=ut&interdum=erat&mauris=id&non=mauris&ligula=vulputate&pellentesque=elementum&ultrices=nullam&phasellus=varius&id=nulla&sapien=facilisi&in=cras&sapien=non&iaculis=velit&congue=nec&vivamus=nisi"
+    }, {
+        "id": "09",
+        "first_name": "Helyn",
+        "phone": "896-816-3981",
+        "email": "hmacdowal8@europa.eu",
+        "address": "PO Box 74935",
+        "age": 2,
+        "photoUrl": "https://miitbeian.gov.cn/aliquet/ultrices/erat/tortor/sollicitudin/mi.png?faucibus=phasellus&orci=id&luctus=sapien&et=in&ultrices=sapien&posuere=iaculis&cubilia=congue&curae=vivamus&duis=metus&faucibus=arcu&accumsan=adipiscing&odio=molestie&curabitur=hendrerit&convallis=at&duis=vulputate&consequat=vitae&dui=nisl&nec=aenean&nisi=lectus&volutpat=pellentesque&eleifend=eget&donec=nunc&ut=donec&dolor=quis&morbi=orci&vel=eget&lectus=orci&in=vehicula&quam=condimentum&fringilla=curabitur&rhoncus=in&mauris=libero&enim=ut&leo=massa&rhoncus=volutpat&sed=convallis&vestibulum=morbi&sit=odio&amet=odio&cursus=elementum&id=eu&turpis=interdum&integer=eu&aliquet=tincidunt&massa=in&id=leo&lobortis=maecenas&convallis=pulvinar&tortor=lobortis&risus=est&dapibus=phasellus&augue=sit&vel=amet&accumsan=erat&tellus=nulla&nisi=tempus&eu=vivamus&orci=in&mauris=felis&lacinia=eu&sapien=sapien&quis=cursus&libero=vestibulum&nullam=proin&sit=eu&amet=mi&turpis=nulla&elementum=ac&ligula=enim&vehicula=in&consequat=tempor&morbi=turpis&a=nec&ipsum=euismod&integer=scelerisque&a=quam&nibh=turpis&in=adipiscing&quis=lorem&justo=vitae"
+    }, {
+        "id": "10",
+        "first_name": "Kris",
+        "phone": "671-752-0476",
+        "email": "kgrigor9@4shared.com",
+        "address": "10th Floor",
+        "age": 6,
+        "photoUrl": "https://vinaora.com/mi/nulla/ac.json?tincidunt=quam&lacus=sollicitudin&at=vitae&velit=consectetuer&vivamus=eget&vel=rutrum&nulla=at&eget=lorem&eros=integer&elementum=tincidunt&pellentesque=ante&quisque=vel&porta=ipsum&volutpat=praesent&erat=blandit&quisque=lacinia&erat=erat&eros=vestibulum&viverra=sed&eget=magna&congue=at&eget=nunc&semper=commodo&rutrum=placerat&nulla=praesent&nunc=blandit&purus=nam&phasellus=nulla&in=integer&felis=pede&donec=justo&semper=lacinia&sapien=eget&a=tincidunt&libero=eget&nam=tempus&dui=vel&proin=pede&leo=morbi&odio=porttitor&porttitor=lorem&id=id&consequat=ligula&in=suspendisse&consequat=ornare&ut=consequat&nulla=lectus&sed=in&accumsan=est&felis=risus&ut=auctor&at=sed&dolor=tristique&quis=in&odio=tempus&consequat=sit&varius=amet&integer=sem&ac=fusce&leo=consequat&pellentesque=nulla&ultrices=nisl&mattis=nunc&odio=nisl&donec=duis&vitae=bibendum&nisi=felis&nam=sed&ultrices=interdum&libero=venenatis&non=turpis&mattis=enim&pulvinar=blandit&nulla=mi&pede=in&ullamcorper=porttitor&augue=pede&a=justo&suscipit=eu&nulla=massa&elit=donec&ac=dapibus"
+    }
+]
+
+module.exports = users;
